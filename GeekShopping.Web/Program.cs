bccfa3d3a99c23f4a16e75c074472a7d4f6cb89c
Tarcio Rodrigo <trodrigo@serel.com.br>
2022-06-01 16:48:16 -0300

    Servico WEB Parte 1

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,6 +1,16 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+        c.BaseAddress = new Uri("http://localhost:5159"));
+
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
