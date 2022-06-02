21d60f602d79988b4a4787d46f28617f0c7af090
Tarcio Rodrigo <trodrigo@serel.com.br>
2022-06-02 01:17:54 -0300

    Implementacao do servico web

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -55,7 +55,7 @@ namespace GeekShopping.ProductAPI.Controllers
             return Ok(product);
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(long id)
         { 
             var status = await _repository.Delete(id);
